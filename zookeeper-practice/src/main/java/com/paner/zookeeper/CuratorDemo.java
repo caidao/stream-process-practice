bafa45c9c3411bf6fb0f580d845a753fd872c072
yiwen.pan <yiwen.pan@ele.me>
2017-06-21 23:33:26 +0800

    curator高级用法

--- zookeeper-practice/src/main/java/com/paner/zookeeper/CuratorDemo.java
+++ zookeeper-practice/src/main/java/com/paner/zookeeper/CuratorDemo.java
@@ -5,6 +5,13 @@ import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.api.BackgroundCallback;
 import org.apache.curator.framework.api.CuratorEvent;
 import org.apache.curator.framework.api.CuratorEventType;
+import org.apache.curator.framework.api.CuratorListener;
+import org.apache.curator.framework.recipes.cache.ChildData;
+import org.apache.curator.framework.recipes.cache.PathChildrenCache;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheListener;
+import org.apache.curator.framework.state.ConnectionState;
+import org.apache.curator.framework.state.ConnectionStateListener;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.AsyncCallback;
 import org.apache.zookeeper.CreateMode;
@@ -32,10 +39,10 @@ public class CuratorDemo {
     public void demo1() throws Exception {
         CuratorFramework czk = createFramework("127.0.0.1:2181");
         czk.start();
-        czk.create().withMode(CreateMode.PERSISTENT).inBackground(createStringCallback).forPath("/test1", "test".getBytes());
-        Thread.sleep(1000);
-        byte[] bytes = czk.getData().forPath("/test1");
-        System.out.println(new String(bytes));
+        czk.create().withProtection().withMode(CreateMode.EPHEMERAL_SEQUENTIAL).inBackground(createStringCallback).forPath("/test_", "test".getBytes());
+        Thread.sleep(100000);
+       // byte[] bytes = czk.getData().forPath("/test1");
+      //  System.out.println(new String(bytes));
        // czk.delete().forPath("/test1");
 
         czk.close();
@@ -55,6 +62,7 @@ public class CuratorDemo {
     @Test
     public void  transaction() throws Exception {
         CuratorFramework czk = createFramework("127.0.0.1:2181");
+        czk.getConnectionStateListenable().addListener(connectionStateListener);
         czk.start();
         czk.inTransaction().check().withVersion(-1).forPath("/test2")
                 .and().create().withMode(CreateMode.EPHEMERAL).forPath("/test2","data".getBytes())
@@ -63,5 +71,54 @@ public class CuratorDemo {
         Thread.sleep(10000);
         czk.close();
     }
+
+
+    @Test
+    /**
+     * Path Cache用来监控一个ZNode的子节点
+     */
+    public void pathCache() throws Exception {
+        String path = "/example/pathCache";
+        CuratorFramework czk = createFramework("127.0.0.1:2181");
+        czk.start();
+        PathChildrenCache cache = new PathChildrenCache(czk,path,true);
+        cache.start();
+        //事件监听器
+        PathChildrenCacheListener cacheListener = new PathChildrenCacheListener() {
+            public void childEvent(CuratorFramework curatorFramework, PathChildrenCacheEvent event) throws Exception {
+                System.out.println("事件类型 = [" + event + "]");
+                if (event.getData() !=null){
+                    System.out.println("节点数据：" + event.getData().getPath() + " = " + new String(event.getData().getData()));
+                }
+            }
+        };
+
+        cache.getListenable().addListener(cacheListener);
+        czk.create().creatingParentContainersIfNeeded().forPath(path + "/test01", "01".getBytes());
+        czk.create().creatingParentContainersIfNeeded().forPath(path+"/test02","02".getBytes());
+        czk.create().creatingParentContainersIfNeeded().forPath(path+"/test03","03".getBytes());
+        Thread.sleep(1000);
+        for (ChildData data:cache.getCurrentData()){
+            System.out.println("getCurrentData:"+data.getPath()+"="+ new String(data.getData()));
+        }
+        Thread.sleep(10000);
+        cache.close();
+        czk.close();
+    }
+
+    private ConnectionStateListener connectionStateListener = new ConnectionStateListener() {
+        public void stateChanged(CuratorFramework client, ConnectionState newState) {
+
+            if (newState == ConnectionState.LOST){
+                //处理session过期
+
+            }
+
+            if (!newState.isConnected()){
+                System.out.println("client = [" + client + "], newState = [" + newState + "]");
+            }
+        }
+    };
+
 
 }
