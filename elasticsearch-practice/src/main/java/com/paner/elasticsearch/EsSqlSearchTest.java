840fb6757afe8fd3da5fa5d980d936bd878dfaac
yiwen.pan <yiwen.pan@ele.me>
2017-09-01 18:58:37 +0800

    增加redis

--- elasticsearch-practice/src/main/java/com/paner/elasticsearch/EsSqlSearchTest.java
+++ elasticsearch-practice/src/main/java/com/paner/elasticsearch/EsSqlSearchTest.java
@@ -1,7 +1,9 @@
 package com.paner.elasticsearch;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.junit.Test;
 import org.nlpcn.es4sql.SearchDao;
@@ -19,18 +21,34 @@ public class EsSqlSearchTest {
     @Test
     public void sqlQuery() throws IOException, SQLFeatureNotSupportedException, SqlParseException {
 
-        String sql = "SELECT * FROM paner_test where order_city_id =23534 ";
+        String sql = "SELECT * FROM bdi_eco_data_index_alias where retail_user_lifecycle ='A10' ";
 
         Client client = ClientTest.getClient();
-
         SearchDao searchDao = new SearchDao(client);
         String dsl = searchDao.explain(sql).explain().explain();
         System.out.println("dsl:" + dsl);
+
 
         SqlElasticRequestBuilder builder =searchDao.explain(sql).explain();
         SearchResponse response = (SearchResponse) builder.get();
         SearchHits hits = response.getHits();
         System.out.println("hits:"+hits.totalHits());
 
+        ObjectMapper mapper = new ObjectMapper();
+//        for (SearchHit hit:hits.getHits()){
+//              writeRedis(hit.getId(),mapper.writeValueAsString(hit.getSource()));
+//        }
+
+    }
+
+    private void writeRedis(String userId,String profileJson){
+        System.out.println("profile:"+profileJson);
+        RedisClient.getInstance().getResource().set(userId,profileJson);
+    }
+
+
+    @Test
+    public  void redisTest(){
+        RedisClient.getInstance().getResource().set("paner","test");
     }
 }
---
+++ elasticsearch-practice/src/main/java/com/paner/elasticsearch/RedisClient.java
@@ -0,0 +1,64 @@
+package com.paner.elasticsearch;
+
+import com.alibaba.druid.support.logging.Log;
+import com.alibaba.druid.support.logging.LogFactory;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class RedisClient {
+        private static final Log LOG = LogFactory.getLog(RedisClient.class);
+        private JedisPool redisPool;
+
+        private RedisClient() {
+            init();
+        }
+
+        public static RedisClient getInstance() {
+            return SingletonHandler.redisClient;
+        }
+
+        /**
+         * get a redis connection from pool
+         */
+        public Jedis getResource() {
+            return redisPool.getResource();
+        }
+
+        private void init() {
+            if (redisPool == null) {
+                JedisPoolConfig config = new JedisPoolConfig();
+                config.setMinIdle(20);
+                config.setMaxIdle(100);
+                config.setMaxTotal(500);
+                config.setBlockWhenExhausted(false);
+                config.setMaxWaitMillis(1000 * 3);
+                config.setTestOnBorrow(false);
+                config.setTestOnReturn(false);
+                config.setTestWhileIdle(false);
+
+                String hostPortParam = "1222:6379";
+                LOG.info("--> hostPortParam = " + hostPortParam);
+                String[] hostPort = hostPortParam.split(":");
+                String host = hostPort[0];
+                int port = 8793;
+                try {
+                    port = Integer.parseInt(hostPort[1]);
+                } catch (Exception e) {
+                    LOG.error( e.getMessage(), e);
+                }
+                redisPool = new JedisPool(config, host, port);
+            }
+            LOG.info("--> Init RedisPool OK");
+            return ;
+        }
+
+        /**
+         * 单例
+         * */
+        private static class SingletonHandler {
+            private static RedisClient redisClient = new RedisClient();
+            private SingletonHandler() { }
+        }
+
+}
